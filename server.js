761be944b8be3319003e72f108a8b1b6bc18dbb8
Er-Saurabh-Gondane <saurabhgondane358@gmail.com>
2026-02-18 12:32:50 +0530

    initial commit with server

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+
+app.listen(PORT ,()=>{
+    console.log(`App is listening on PORT ${PORT}`);  
+})
